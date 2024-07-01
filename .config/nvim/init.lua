b4e4c226cfcad48356e26ecbb4136502e4812fbe
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-07-01 13:15:51 -0500

    format and add lua lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,8 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("inoremap jj <Esc>")
+vim.cmd "set expandtab"
+vim.cmd "set tabstop=2"
+vim.cmd "set softtabstop=2"
+vim.cmd "set shiftwidth=2"
+vim.cmd "inoremap jj <Esc>"
 vim.g.mapleader = " "
 
-require("config.lazy")
+require "config.lazy"
