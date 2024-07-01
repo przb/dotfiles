207bcb1e3a68a5628b21d08d561380f5c17dc3d1
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-07-01 13:47:06 -0500

    move lsp keymaps to mapping file

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -2,3 +2,8 @@ local map = vim.keymap.set
 
 map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
 map("n", "<leader>rn", "<cmd>set rnu!<CR><cmd>set nu!<CR>", { desc = "toggle relative number" })
+
+-- lsp settings
+map("n", "K", vim.lsp.buf.hover, {})
+map("n", "gd", vim.lsp.buf.definition, {})
+map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -12,10 +12,6 @@ return {
       lspconfig.lua_ls.setup {}
       -- lspconfig.tsserver.setup {}
       lspconfig.rust_analyzer.setup {}
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
   {
