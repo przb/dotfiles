f3ae584294a11995f548990fe473105f822d1abf
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-07-01 18:08:16 -0500

    remove unused options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,7 +1,6 @@
 require "nvchad.options"
 
 -- add yours here!
-vim.cmd("set ")
 local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 o.nu = true
@@ -15,10 +14,10 @@ o.smartindent = true
 o.wrap = false
 
 -- undo tree settings
-o.swapfile = false
-o.backup = false
-o.undodir = os.getenv("HOME") .. "/.vim/undodir"
-o.undofile = true
+-- o.swapfile = false
+-- o.backup = false
+-- o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- o.undofile = true
 
 o.hlsearch = false
 o.incsearch =  true
