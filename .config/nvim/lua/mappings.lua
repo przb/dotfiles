21afd89b855a1c7cf78f400fa052f30e608fced4
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-07-01 19:29:35 -0500

    add some more primeagen mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -9,3 +9,10 @@ map("i", "jk", "<ESC>")
 map("i", "jj", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- enable moving of lines when in visual mode
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '>-2<CR>gv=gv")
+
+-- enable pasting over another word in visual mode. With this mapping, the current paste buffer is not lost
+map("x", "<leader>p", '"_dP')
