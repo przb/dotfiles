833da9f145aaa3e734feeb8ebd77cc7e5d841500
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-08-06 16:11:15 -0500

    add svelte and javascript lsp

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -5,12 +5,12 @@ local on_init = configs.on_init
 local capabilities = configs.capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "rust_analyzer", "pyright" }
+local servers = { "html", "cssls", "clangd", "rust_analyzer", "pyright", "svelte", "eslint" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_init = on_init,
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+    lspconfig[lsp].setup {
+        on_init = on_init,
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
 end
